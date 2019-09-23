edc8abbf7d35ba2852390836c1440f4a62ae6673
deadcode <josephsonsjoseph@gmail.com>
2019-09-22 21:01:43 -0700

    "Wrote competition drop down list"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component} from "react";
 import "./App.css";
+import CompDropDown from "./components/CompDropDown.jsx"
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <h1> Welcome to No Spoilers </h1>
+        <div>
+          <CompDropDown />
+        </div>
       </div>
     );
   }
---
+++ src/components/CompDropDown.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function CompDropDown (props) {
+  return (
+    <div>
+      <label for="competition">Select a competition:</label>
+      <select id="comp-list">
+        <option value="">--Please choose a competition--</option>
+        <option value="CL">Champions League</option>
+        <option value="PL">Premier League</option>
+        <option value="SA">Serie A</option>
+        <option value="PD">La Liga</option>
+        <option value="BL1">Bundesliga</option>
+        <option value="FL1">Ligue 1</option>
+      </select>
+    </div>
+  )
+}
+
+export default CompDropDown;
