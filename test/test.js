78e8663c3703de34cf26a0b8e25923b332eddea9
deadcode <josephsonsjoseph@gmail.com>
2019-09-25 16:11:16 -0700

    "Wrote preliminary tests for data formatting"

--- test/test.js
+++ test/test.js
@@ -1,11 +1,45 @@
 var expect = require('chai').expect;
 var myFunc = require('../models/dataFormatting.js');
+var testData = require('../test_data.js');
 
-describe('Test test', function() {
-  describe('My test', function() {
-    it('should pass this simple test', function() {
-      expect(myFunc.rankMatches).to.be.an('function');
-      expect(myFunc.rankMatches).to.not.be.an('object');
+describe('Unit tests', function() {
+  describe('Data Format', function() {
+    it('data should be in the correct format', function() {
+      expect(testData.fixtures).to.be.an('array');
+      expect(testData.statistics).to.be.an('array');
     })
+  })
+  describe('Data formatting functions', function() {
+    const cleanFixtures = testData.fixtures;
+    const cleanStatistics = testData.statistics;
+
+    var fixtures = testData.fixtures;
+    var statistics = testData.statistics;
+
+    beforeEach(function() {
+      fixtures = cleanFixtures;
+      statistics = cleanStatistics;
+    })
+
+    describe('statsDecorator Function', function() {
+      let fixture = fixtures[0];
+      let statisticsObj = statistics[0];
+
+      it('Should add a \'stats\' property to test data', function() {
+        myFunc.statsDecorator(fixture, statisticsObj);
+        expect(fixture).to.have.ownPropertyDescriptor('stats');
+      })
+      it('Should add a \'ranking\' property to test data', function() {
+        expect(fixture).to.have.ownPropertyDescriptor('ranking');
+      })
+      it('Should add all relevant properies to the stats object', function() {
+        expect(fixture.stats).to.have.ownPropertyDescriptor('goals');
+        expect(fixture.stats).to.have.ownPropertyDescriptor('numShotsOnGoal');
+        expect(fixture.stats).to.have.ownPropertyDescriptor('saves');
+        expect(fixture.stats).to.have.ownPropertyDescriptor('possDiff');
+      })
+    })
+
+
   })
 })
---
+++ test_data.js
@@ -0,0 +1,614 @@
+var fixtures = [{
+    "fixture_id": 135778,
+    "league_id": 1,
+    "event_date": "2018-06-15T12:00:00+00:00",
+    "event_timestamp": 1529064000,
+    "firstHalfStart": 1529064000,
+    "secondHalfStart": 1529067600,
+    "round": "Group Stage - 1",
+    "status": "Match Finished",
+    "statusShort": "FT",
+    "elapsed": 90,
+    "venue": "Ekaterinburg Arena (Ekaterinburg)",
+    "referee": null,
+    "homeTeam": {
+      "team_id": 32,
+      "team_name": "Egypt",
+      "logo": "https://media.api-football.com/teams/32.png"
+    },
+    "awayTeam": {
+      "team_id": 7,
+      "team_name": "Uruguay",
+      "logo": "https://media.api-football.com/teams/7.png"
+    },
+    "goalsHomeTeam": 0,
+    "goalsAwayTeam": 1,
+    "score": {
+      "halftime": "0-0",
+      "fulltime": "0-1",
+      "extratime": null,
+      "penalty": null
+    },
+    "events": [{
+        "elapsed": 50,
+        "team_id": 32,
+        "teamName": "Egypt",
+        "player_id": 2663,
+        "player": "Tarek Hamed",
+        "type": "subst",
+        "detail": "Sam Morsy"
+      },
+      {
+        "elapsed": 58,
+        "team_id": 7,
+        "teamName": "Uruguay",
+        "player_id": 2614,
+        "player": "Nahitan Nández",
+        "type": "subst",
+        "detail": "Carlos Sánchez"
+      },
+      {
+        "elapsed": 59,
+        "team_id": 7,
+        "teamName": "Uruguay",
+        "player_id": 2612,
+        "player": "Giorgian De Arrascaeta",
+        "type": "subst",
+        "detail": "Cristian Rodríguez"
+      },
+      {
+        "elapsed": 63,
+        "team_id": 32,
+        "teamName": "Egypt",
+        "player_id": 16828,
+        "player": "Marwan Mohsen",
+        "type": "subst",
+        "detail": "Kahraba"
+      },
+      {
+        "elapsed": 82,
+        "team_id": 32,
+        "teamName": "Egypt",
+        "player_id": 2667,
+        "player": "Amr Warda",
+        "type": "subst",
+        "detail": "Ramadan Sobhi"
+      },
+      {
+        "elapsed": 87,
+        "team_id": 7,
+        "teamName": "Uruguay",
+        "player_id": 211,
+        "player": "Matías Vecino",
+        "type": "subst",
+        "detail": "Lucas Torreira"
+      },
+      {
+        "elapsed": 89,
+        "team_id": 7,
+        "teamName": "Uruguay",
+        "player_id": 31,
+        "player": "José Giménez",
+        "type": "Goal",
+        "detail": "Normal Goal"
+      },
+      {
+        "elapsed": 90,
+        "team_id": 32,
+        "teamName": "Egypt",
+        "player_id": 19149,
+        "player": "Ahmed Hegazy",
+        "type": "Card",
+        "detail": "Yellow Card"
+      },
+      {
+        "elapsed": 90,
+        "team_id": 32,
+        "teamName": "Egypt",
+        "player_id": 19558,
+        "player": "Sam Morsy",
+        "type": "Card",
+        "detail": "Yellow Card"
+      }
+    ]
+
+  },
+  {
+    "fixture_id": 135779,
+    "league_id": 1,
+    "event_date": "2018-06-15T15:00:00+00:00",
+    "event_timestamp": 1529074800,
+    "firstHalfStart": 1529074800,
+    "secondHalfStart": 1529078400,
+    "round": "Group Stage - 1",
+    "status": "Match Finished",
+    "statusShort": "FT",
+    "elapsed": 90,
+    "venue": "Saint-Petersburg Stadium (Sankt-Peterburg (St. Petersburg))",
+    "referee": null,
+    "homeTeam": {
+      "team_id": 31,
+      "team_name": "Morocco",
+      "logo": "https://media.api-football.com/teams/31.png"
+    },
+    "awayTeam": {
+      "team_id": 22,
+      "team_name": "Iran",
+      "logo": "https://media.api-football.com/teams/22.png"
+    },
+    "goalsHomeTeam": 0,
+    "goalsAwayTeam": 1,
+    "score": {
+      "halftime": "0-0",
+      "fulltime": "0-1",
+      "extratime": null,
+      "penalty": null
+    },
+    "events": [{
+        "elapsed": 10,
+        "team_id": 22,
+        "teamName": "Iran",
+        "player_id": 2696,
+        "player": "Masoud Shojaei",
+        "type": "Card",
+        "detail": "Yellow Card"
+      },
+      {
+        "elapsed": 34,
+        "team_id": 31,
+        "teamName": "Morocco",
+        "player_id": 2713,
+        "player": "Karim El Ahmadi",
+        "type": "Card",
+        "detail": "Yellow Card"
+      },
+      {
+        "elapsed": 47,
+        "team_id": 22,
+        "teamName": "Iran",
+        "player_id": 2700,
+        "player": "Alireza Jahanbakhsh",
+        "type": "Card",
+        "detail": "Yellow Card"
+      },
+      {
+        "elapsed": 68,
+        "team_id": 22,
+        "teamName": "Iran",
+        "player_id": 2696,
+        "player": "Masoud Shojaei",
+        "type": "subst",
+        "detail": "Mehdi Taremi"
+      },
+      {
+        "elapsed": 76,
+        "team_id": 31,
+        "teamName": "Morocco",
+        "player_id": 2719,
+        "player": "Nordin Amrabat",
+        "type": "subst",
+        "detail": "Sofyan Amrabat"
+      },
+      {
+        "elapsed": 77,
+        "team_id": 31,
+        "teamName": "Morocco",
+        "player_id": 2722,
+        "player": "Ayoub El Kaabi",
+        "type": "subst",
+        "detail": "Aziz Bouhaddouz"
+      },
+      {
+        "elapsed": 80,
+        "team_id": 22,
+        "teamName": "Iran",
+        "player_id": 2694,
+        "player": "Omid Ebrahimi",
+        "type": "subst",
+        "detail": "Majid Hosseini"
+      },
+      {
+        "elapsed": 82,
+        "team_id": 31,
+        "teamName": "Morocco",
+        "player_id": 412,
+        "player": "Amine Harit",
+        "type": "subst",
+        "detail": "Manuel Da Costa"
+      },
+      {
+        "elapsed": 85,
+        "team_id": 22,
+        "teamName": "Iran",
+        "player_id": 2700,
+        "player": "Alireza Jahanbakhsh",
+        "type": "subst",
+        "detail": "Saman Ghoddos"
+      },
+      {
+        "elapsed": 90,
+        "team_id": 22,
+        "teamName": "Iran",
+        "player_id": 2698,
+        "player": "Karim Ansarifard",
+        "type": "Card",
+        "detail": "Yellow Card"
+      },
+      {
+        "elapsed": 90,
+        "team_id": 22,
+        "teamName": "Iran",
+        "player_id": null,
+        "player": "Aziz Bouhaddouz",
+        "type": "Goal",
+        "detail": "Own Goal"
+      }
+    ]
+  },
+  {
+    "fixture_id": 135780,
+    "league_id": 1,
+    "event_date": "2018-06-15T18:00:00+00:00",
+    "event_timestamp": 1529085600,
+    "firstHalfStart": 1529085600,
+    "secondHalfStart": 1529089200,
+    "round": "Group Stage - 1",
+    "status": "Match Finished",
+    "statusShort": "FT",
+    "elapsed": 90,
+    "venue": "Olimpiyskiy Stadion Fisht (Sochi)",
+    "referee": null,
+    "homeTeam": {
+      "team_id": 27,
+      "team_name": "Portugal",
+      "logo": "https://media.api-football.com/teams/27.png"
+    },
+    "awayTeam": {
+      "team_id": 9,
+      "team_name": "Spain",
+      "logo": "https://media.api-football.com/teams/9.png"
+    },
+    "goalsHomeTeam": 3,
+    "goalsAwayTeam": 3,
+    "score": {
+      "halftime": "2-1",
+      "fulltime": "3-3",
+      "extratime": null,
+      "penalty": null
+    },
+    "events": [{
+        "elapsed": 4,
+        "team_id": 27,
+        "teamName": "Portugal",
+        "player_id": 874,
+        "player": "Cristiano Ronaldo",
+        "type": "Goal",
+        "detail": "Penalty"
+      },
+      {
+        "elapsed": 17,
+        "team_id": 9,
+        "teamName": "Spain",
+        "player_id": 144,
+        "player": "Sergio Busquets",
+        "type": "Card",
+        "detail": "Yellow Card"
+      },
+      {
+        "elapsed": 24,
+        "team_id": 9,
+        "teamName": "Spain",
+        "player_id": 54,
+        "player": "Diego Costa",
+        "type": "Goal",
+        "detail": "Normal Goal"
+      },
+      {
+        "elapsed": 28,
+        "team_id": 27,
+        "teamName": "Portugal",
+        "player_id": 1485,
+        "player": "Bruno Fernandes",
+        "type": "Card",
+        "detail": "Yellow Card"
+      },
+      {
+        "elapsed": 44,
+        "team_id": 27,
+        "teamName": "Portugal",
+        "player_id": 874,
+        "player": "Cristiano Ronaldo",
+        "type": "Goal",
+        "detail": "Normal Goal"
+      },
+      {
+        "elapsed": 55,
+        "team_id": 9,
+        "teamName": "Spain",
+        "player_id": 54,
+        "player": "Diego Costa",
+        "type": "Goal",
+        "detail": "Normal Goal"
+      },
+      {
+        "elapsed": 58,
+        "team_id": 9,
+        "teamName": "Spain",
+        "player_id": 735,
+        "player": "Nacho Fernández",
+        "type": "Goal",
+        "detail": "Normal Goal"
+      },
+      {
+        "elapsed": 68,
+        "team_id": 27,
+        "teamName": "Portugal",
+        "player_id": 1485,
+        "player": "Bruno Fernandes",
+        "type": "subst",
+        "detail": "João Mário"
+      },
+      {
+        "elapsed": 69,
+        "team_id": 27,
+        "teamName": "Portugal",
+        "player_id": 636,
+        "player": "Bernardo Silva",
+        "type": "subst",
+        "detail": "Ricardo Quaresma"
+      },
+      {
+        "elapsed": 70,
+        "team_id": 9,
+        "teamName": "Spain",
+        "player_id": 33243,
+        "player": "Andrés Iniesta",
+        "type": "subst",
+        "detail": "Thiago Alcántara"
+      },
+      {
+        "elapsed": 77,
+        "team_id": 9,
+        "teamName": "Spain",
+        "player_id": 54,
+        "player": "Diego Costa",
+        "type": "subst",
+        "detail": "Iago Aspas"
+      },
+      {
+        "elapsed": 80,
+        "team_id": 27,
+        "teamName": "Portugal",
+        "player_id": 925,
+        "player": "Gonçalo Guedes",
+        "type": "subst",
+        "detail": "André Silva"
+      },
+      {
+        "elapsed": 86,
+        "team_id": 9,
+        "teamName": "Spain",
+        "player_id": 634,
+        "player": "David Silva",
+        "type": "subst",
+        "detail": "Lucas Vázquez"
+      },
+      {
+        "elapsed": 88,
+        "team_id": 27,
+        "teamName": "Portugal",
+        "player_id": 874,
+        "player": "Cristiano Ronaldo",
+        "type": "Goal",
+        "detail": "Normal Goal"
+      }
+    ]
+  }
+]
+
+var statistics = [{
+    "Shots on Goal": {
+      "home": "3",
+      "away": "5"
+    },
+    "Shots off Goal": {
+      "home": "3",
+      "away": "6"
+    },
+    "Total Shots": {
+      "home": "8",
+      "away": "15"
+    },
+    "Blocked Shots": {
+      "home": "2",
+      "away": "4"
+    },
+    "Shots insidebox": {
+      "home": "4",
+      "away": "8"
+    },
+    "Shots outsidebox": {
+      "home": "4",
+      "away": "7"
+    },
+    "Fouls": {
+      "home": "12",
+      "away": "6"
+    },
+    "Corner Kicks": {
+      "home": "0",
+      "away": "5"
+    },
+    "Offsides": {
+      "home": "1",
+      "away": "1"
+    },
+    "Ball Possession": {
+      "home": "41%",
+      "away": "59%"
+    },
+    "Yellow Cards": {
+      "home": "2",
+      "away": "0"
+    },
+    "Red Cards": {
+      "home": "0",
+      "away": "0"
+    },
+    "Goalkeeper Saves": {
+      "home": "4",
+      "away": "3"
+    },
+    "Total passes": {
+      "home": "424",
+      "away": "588"
+    },
+    "Passes accurate": {
+      "home": "313",
+      "away": "503"
+    },
+    "Passes %": {
+      "home": "74%",
+      "away": "86%"
+    }
+
+  },
+
+  {
+    "Shots on Goal": {
+      "home": "3",
+      "away": "2"
+    },
+    "Shots off Goal": {
+      "home": "6",
+      "away": "6"
+    },
+    "Total Shots": {
+      "home": "13",
+      "away": "9"
+    },
+    "Blocked Shots": {
+      "home": "4",
+      "away": "1"
+    },
+    "Shots insidebox": {
+      "home": "9",
+      "away": "6"
+    },
+    "Shots outsidebox": {
+      "home": "4",
+      "away": "4"
+    },
+    "Fouls": {
+      "home": "22",
+      "away": "14"
+    },
+    "Corner Kicks": {
+      "home": "5",
+      "away": "2"
+    },
+    "Offsides": {
+      "home": null,
+      "away": null
+    },
+    "Ball Possession": {
+      "home": "67%",
+      "away": "33%"
+    },
+    "Yellow Cards": {
+      "home": "1",
+      "away": "3"
+    },
+    "Red Cards": {
+      "home": "0",
+      "away": "0"
+    },
+    "Goalkeeper Saves": {
+      "home": "2",
+      "away": "3"
+    },
+    "Total passes": {
+      "home": "461",
+      "away": "218"
+    },
+    "Passes accurate": {
+      "home": "374",
+      "away": "127"
+    },
+    "Passes %": {
+      "home": "81%",
+      "away": "58%"
+    }
+  },
+  {
+    "Shots on Goal": {
+      "home": "3",
+      "away": "6"
+    },
+    "Shots off Goal": {
+      "home": "3",
+      "away": "5"
+    },
+    "Total Shots": {
+      "home": "9",
+      "away": "13"
+    },
+    "Blocked Shots": {
+      "home": "3",
+      "away": "2"
+    },
+    "Shots insidebox": {
+      "home": "3",
+      "away": "9"
+    },
+    "Shots outsidebox": {
+      "home": "6",
+      "away": "4"
+    },
+    "Fouls": {
+      "home": "12",
+      "away": "10"
+    },
+    "Corner Kicks": {
+      "home": "4",
+      "away": "5"
+    },
+    "Offsides": {
+      "home": "1",
+      "away": "3"
+    },
+    "Ball Possession": {
+      "home": "33%",
+      "away": "67%"
+    },
+    "Yellow Cards": {
+      "home": "1",
+      "away": "1"
+    },
+    "Red Cards": {
+      "home": "0",
+      "away": "0"
+    },
+    "Goalkeeper Saves": {
+      "home": "3",
+      "away": "0"
+    },
+    "Total passes": {
+      "home": "372",
+      "away": "759"
+    },
+    "Passes accurate": {
+      "home": "307",
+      "away": "700"
+    },
+    "Passes %": {
+      "home": "83%",
+      "away": "92%"
+    }
+  }
+]
+
+module.exports = {
+  fixtures,
+  statistics
+};
