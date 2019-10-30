67ad288a2085e218323f4cf376dc9e58b28dd64e
deadcode <josephsonsjoseph@gmail.com>
2019-10-30 16:24:11 -0700

    "Setup sass and bootstrap"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
+import 'bootstrap';
+import '../scss/app.scss';
+
 ReactDOM.render( < App / > , document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,9 @@ module.exports = {
       options: {
         presets: ["@babel/env"]
       }
+    }, {
+      test: /\.(scss)$/,
+      use: [{loader: 'style-loader'}, {loader: 'css-loader'}, {loader: 'postcss-loader', options: {plugins: function() {return [require('autoprefixer')];}}}, {loader: 'sass-loader'}]
     }]
   },
   resolve: {
